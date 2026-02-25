46f8aeb086177369a1428f395d9eee009a892812
Emily Koskinen <koskinen.emily@gmail.com>
2026-02-25 13:07:03 +0100

    added 3 linear mixed effects models

--- EM&LM Beginnings.R
+++ EM&LM Beginnings.R
@@ -1,26 +1,26 @@
-load("~/Documents/EM&LM/Group Assignment/Group_7.RData")
+load("Group_7.RData")
 
 library(ggplot2)
 library(dplyr)
 
 # Very basic probing of the shape
 #----------------------
-for (i in 0:2){
+for (i in 0:2) {
   print(mean(DF$smoking_status == i))
 }
 
-for (j in c("male", "female")){
+for (j in c("male", "female")) {
   print(mean(DF$sex == j))
 }
 
-ggplot(data = DF, aes(x = Age, colour = "red")) + 
+ggplot(data = DF, aes(x = Age, colour = "red")) +
   geom_histogram(stat = "bin", binwidth = 1)
 
-ggplot(data = DF, aes(x = Visit, colour = "blue")) + 
+ggplot(data = DF, aes(x = Visit, colour = "blue")) +
   geom_histogram(stat = "bin", binwidth = 1)
 
 # for (k in 0:7){
-# print(ggplot(data = DF[DF$Visit == k, ], aes_string(x = "Age", y = "UPDRS")) + 
+# print(ggplot(data = DF[DF$Visit == k, ], aes_string(x = "Age", y = "UPDRS")) +
 #   geom_bar(stat = "identity") +
 #   labs(title = k))
 # }
@@ -30,32 +30,77 @@ ggplot(data = DF, aes(x = Visit, colour = "blue")) +
 # Male = 0, Female = 1
 
 df <- DF
-df <- df %>% 
-  mutate(across(sex, function(x){
-    case_when(
-      x == "male" ~ 0,
-      x == "female" ~ 1)}
-  ))
+df <- df %>%
+  mutate(across(sex, function(x) {
+    case_when(x == "male" ~ 0, x == "female" ~ 1)
+  }))
 # Centre age to get more meaningful results. Need to understand this better.
 df$age_centred <- scale(df$Age, center = TRUE, scale = FALSE)
 
 # What was done on slide 121
 library(nlme)
-# For highly unbalanced data collected at diﬀerent occassions per subject: Continuous AR1, 
+# For highly unbalanced data collected at diﬀerent occassions per subject: Continuous AR1,
 # Exponential serial correlation, Gaussian serial correlation.
 
 # Cont. AR1
-model.car1 <- gls(UPDRS ~ smoking_status*age_centred+smoking_status*sex, correlation = corCAR1(form = ~ 1 | id),
-                  data = df, method = "REML")
+model.car1 <- gls(
+  UPDRS ~ Visit + smoking_status * age_centred + smoking_status * sex,
+  correlation = corCAR1(form = ~ 1 | id),
+  data = df,
+  method = "REML"
+)
 
-model.exp <- gls(UPDRS ~ smoking_status+age_centred+sex, correlation = corExp(form = ~ 1 | id),
-                  data = df, method = "REML")
+model.exp <- gls(
+  UPDRS ~ Visit + smoking_status + age_centred + sex,
+  correlation = corExp(form = ~ 1 | id),
+  data = df,
+  method = "REML"
+)
 
-model.gaus <- gls(UPDRS ~ smoking_status+age_centred+sex, correlation = corGaus(form = ~ 1 | id),
-                  data = df, method = "REML")
+model.gaus <- gls(
+  UPDRS ~ Visit + smoking_status + age_centred + sex,
+  correlation = corGaus(form = ~ 1 | id),
+  data = df,
+  method = "REML"
+)
 
 summary(model.car1)
 summary(model.exp)
 summary(model.gaus)
 # How am I getting that age isn't a significant factor in Parkinson's?
 # I need random effects models.
+
+# linear mixed effects model for random intercepts and random slopes
+model.lme1 <- lme(
+  UPDRS ~ smoking_status + Visit + Age + sex + Visit:smoking_status,
+  data = DF,
+  random = ~ Visit | id,
+  method = "REML"
+)
+## gives an error???
+
+
+# linear mixed effects model for random intercepts
+model.lme2 <- lme(
+  UPDRS ~ smoking_status + Visit + Age + sex + Visit:smoking_status,
+  data = DF,
+  random = ~ 1 | id,
+  method = "REML"
+)
+summary(model.lme2)
+
+# linear mixed effects model for uncorrelated random intercepts and random slopes
+
+model.lme3 <- lme(
+  UPDRS ~ smoking_status + Visit + Age + sex + Visit:smoking_status,
+  data = DF,
+  random = list(id = pdDiag(form = ~ id)),
+  method = "REML"
+)
+summary(model.lme3)
+## also error???
+
+
+
+
+
